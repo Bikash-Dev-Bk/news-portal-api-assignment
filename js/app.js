b162b325fbff78d258220d05b669898ce51937ce
bikash-dev <bikash.dev.bk@gmail.com>
2022-09-03 05:29:54 +0600

    categories added

--- js/app.js
+++ js/app.js
@@ -1,16 +1,22 @@
 const newsCategories = () =>{
     const url = 'https://openapi.programming-hero.com/api/news/categories';
-
     fetch(url)
     .then(res => res.json())
     .then(data => displayNewsCategories(data.data.news_category))
 }
 
 const displayNewsCategories = (categories) =>{
-    // console.log(categories);
+
+    const newsCategoryContainer = document.getElementById('news-category');
+
     categories.forEach( category => {
-        console.log(category.category_name);
-        
+        const newsCategoryDiv = document.createElement('div');
+        newsCategoryDiv.classList.add('col');
+        newsCategoryDiv.innerHTML = `
+        <div class="fw-semibold">${category.category_name}</div>
+        `;
+
+        newsCategoryContainer.appendChild(newsCategoryDiv);
     });
 }
 
